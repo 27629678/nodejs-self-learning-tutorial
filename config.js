6886038aeafdeac3d823e749e9f353ea7e784540
yuxiaohua <hzyuxiaohua@corp.netease.com>
2018-07-12 10:30:06 +0800

    change logs directory

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   log4js: {
-    appenders: { file: { type: 'file', filename: 'access.log' },
+    appenders: { file: { type: 'file', filename: './logs/access.log' },
                  console: { type: 'console' } },
     categories: { default: { appenders: ['file', 'console'], level: 'debug' } }
   }
